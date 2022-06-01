4574910792f1c100d305e3ad5a7b052a6b3a17dc
ashrafstakala <ashrafstakala@gmail.com>
2022-06-01 21:48:27 +0200

    determine execution path

--- packages/local-api/dist/index.js
+++ packages/local-api/dist/index.js
@@ -5,18 +5,21 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.serve = void 0;
 const express_1 = __importDefault(require("express"));
+const http_proxy_middleware_1 = require("http-proxy-middleware");
 const path_1 = __importDefault(require("path"));
-const serve = (port, filename, dir) => {
+const serve = (port, filename, dir, useProxy) => {
     const app = (0, express_1.default)();
-    const packagePath = require.resolve('local-client/build/index.html');
-    app.use(express_1.default.static(path_1.default.dirname(packagePath)));
-    // app.use(
-    //   createProxyMiddleware({
-    //     target: 'http://localhost:3000',
-    //     ws: true,
-    //     logLevel: 'silent',
-    //   })
-    // );
+    if (useProxy) {
+        app.use((0, http_proxy_middleware_1.createProxyMiddleware)({
+            target: 'http://localhost:3000',
+            ws: true,
+            logLevel: 'silent',
+        }));
+    }
+    else {
+        const packagePath = require.resolve('local-client/build/index.html');
+        app.use(express_1.default.static(path_1.default.dirname(packagePath)));
+    }
     return new Promise((resolve, reject) => {
         app.listen(port, resolve).on('error', reject);
     });
